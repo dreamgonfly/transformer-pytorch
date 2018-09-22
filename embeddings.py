52692e2543bbb9ea3ff1f08b7d47a9869e0c4d3c
Yongrae Jo <dreamgonfly@gmail.com>
2018-09-23 01:10:35 +0900

    Positional encoding implemented

---
+++ embeddings.py
@@ -0,0 +1,45 @@
+import torch
+from torch import nn
+import math
+
+
+class PositionalEncoding(nn.Module):
+    """
+    Implements the sinusoidal positional encoding for
+    non-recurrent neural networks.
+
+    Implementation based on "Attention Is All You Need"
+    :cite:`DBLP:journals/corr/VaswaniSPUJGKP17`
+
+    Args:
+       dropout (float): dropout parameter
+       dim (int): embedding size
+    """
+
+    def __init__(self, num_embeddings, embedding_dim, dim, dropout_prob=0., padding_idx=0, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+
+        pe = torch.zeros(max_len, dim)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp((torch.arange(0, dim, 2) *
+                             -(math.log(10000.0) / dim)).float())
+        pe[:, 0::2] = torch.sin(position.float() * div_term)
+        pe[:, 1::2] = torch.cos(position.float() * div_term)
+        pe = pe.unsqueeze(1)
+
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.embbedding = nn.Embedding(num_embeddings, embedding_dim, padding_idx=padding_idx)
+        self.register_buffer('pe', pe)
+        self.dropout = nn.Dropout(p=dropout_prob)
+        self.dim = dim
+
+    def forward(self, x, step=None):
+        x = self.embbedding(x)
+        x = x * math.sqrt(self.dim)
+        if step is None:
+            x = x + self.pe[:x.size(0)]
+        else:
+            x = x + self.pe[step]
+        x = self.dropout(x)
+        return x
