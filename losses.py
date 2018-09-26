9e90fd7f672db4ecd19c425ed4dc3ecc63fb8ddb
Yongrae Jo <dreamgonfly@gmail.com>
2018-09-27 02:28:26 +0900

    Update label smoothing loss

--- losses.py
+++ losses.py
@@ -4,6 +4,7 @@ from torch.nn import KLDivLoss
 
 PAD_INDEX = 0
 
+
 class MaskedCrossEntropyLoss:
 
     def __init__(self, device):
@@ -37,45 +38,42 @@ class MaskedCrossEntropyLoss:
 
 
 class LabelSmoothingLoss(nn.Module):
+    """
+    With label smoothing,
+    KL-divergence between q_{smoothed ground truth prob.}(w)
+    and p_{prob. computed by model}(w) is minimized.
+    """
+    def __init__(self, label_smoothing, vocabulary_size, padding_idx=0):
+        assert 0.0 < label_smoothing <= 1.0
 
-    def __init__(self, label_smoothing, vocabulary_size):
         super(LabelSmoothingLoss, self).__init__()
-        assert 0.0 <= label_smoothing <= 1.0
-        self.log_softmax = nn.LogSoftmax(dim=-1)
 
-        if label_smoothing > 0:
-            # When label smoothing is turned on,
-            # KL-divergence between q_{smoothed ground truth prob.}(w)
-            # and p_{prob. computed by model}(w) is minimized.
-            # If label smoothing value is set to zero, the loss
-            # is equivalent to NLLLoss or CrossEntropyLoss.
-            # All non-true labels are uniformly set to low-confidence.
-            self.criterion = nn.KLDivLoss(size_average=False)
-            one_hot = torch.randn(1, vocabulary_size)
-            one_hot.fill_(label_smoothing / (vocabulary_size - 2))  # exclude pad and true label
-            one_hot[0][PAD_INDEX] = 0
-            self.register_buffer('one_hot', one_hot)
-        else:
-            weight = torch.ones(vocabulary_size)
-            weight[PAD_INDEX] = 0
-            self.criterion = nn.NLLLoss(weight, size_average=False)
+        self.padding_idx = padding_idx
+        self.log_softmax = nn.LogSoftmax(dim=-1)
+        self.criterion = nn.KLDivLoss(reduction='sum')
+
+        smoothing_value = label_smoothing / (vocabulary_size - 2)  # exclude pad and true label
+        one_hot = torch.full((vocabulary_size,), smoothing_value)
+        one_hot[self.padding_idx] = 0
+        self.register_buffer('one_hot', one_hot.unsqueeze(0))
+
         self.confidence = 1.0 - label_smoothing
 
-    def forward(self, outputs, targets, targets_lengths):
-        outputs_log_softmax = self.log_softmax(outputs)
+    def forward(self, outputs, targets, tmp):
+        """
+        outputs (FloatTensor): batch_size x seq_len x n_classes
+        targets (LongTensor): batch_size x seq_len
+        """
+        batch_size, seq_len, vocabulary_size = outputs.size()
 
-        outputs_flat = outputs_log_softmax.view(-1, outputs_log_softmax.size(2))
-        targets_flat = targets.view(-1)
-        if self.confidence < 1:
-            targets_detached = targets_flat.detach()
-            mask = torch.nonzero(targets_detached == PAD_INDEX).squeeze(-1)
-            tmp_ = self.one_hot.repeat(targets_flat.size(0), 1)
-            tmp_.scatter_(1, targets_detached.unsqueeze(1), self.confidence)
-            if mask.size(0) > 0:
-                tmp_.index_fill_(0, mask, 0)
-            targets_flat = tmp_
-        loss = self.criterion(outputs_flat, targets_flat)
+        outputs = self.log_softmax(outputs)
+        outputs = outputs.view(batch_size * seq_len, vocabulary_size)
+        targets = targets.view(batch_size * seq_len)
+        model_prob = self.one_hot.repeat(targets.size(0), 1)
+        model_prob.scatter_(1, targets.unsqueeze(1), self.confidence)
+        model_prob.masked_fill_((targets == self.padding_idx).unsqueeze(1), 0)
 
-        count = (targets_detached != PAD_INDEX).sum().item()
+        loss = self.criterion(outputs, model_prob)
+        count = (targets != self.padding_idx).sum().item()
 
         return loss, count
